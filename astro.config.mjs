45842d5e7339523088e3156fb1dca544717597f9
Fernando David Carela <fernandodavidcarela@gmail.com>
2026-02-04 17:03:58 -0400

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://fernan-15.github.io',
   integrations: [mdx(), sitemap()],
 
   vite: {
